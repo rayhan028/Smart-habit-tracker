5f8879e3308b28c7cc2160b9ab0e9f9cba626e89
rayhan028 <rayhanm024@gmail.com>
2025-11-13 18:01:18 +0100

    Create User.java

---
+++ src/main/java/com/habittracker/model/User.java
@@ -0,0 +1,17 @@
+package com.habittracker.model;
+
+public class User {
+    private final int id;
+    private final String username;
+    private final String passwordHash;
+
+    public User(int id, String username, String passwordHash) {
+        this.id = id;
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public int getId() { return id; }
+    public String getUsername() { return username; }
+    public String getPasswordHash() { return passwordHash; }
+}
